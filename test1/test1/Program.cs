ecf51e243caf9511d346667bd52cdeea9cda3c45
leonguyenontario <leohpnguyen@gmail.com>
2019-12-27 22:46:08 -0500

    1st

---
+++ test1/test1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int o = 9;
+
+            Console.WriteLine(o);
+        }
+    }
+}
